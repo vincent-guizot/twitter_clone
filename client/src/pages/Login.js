f2bafe0af4538315189ec8fe46ccaed56d88bdc5
susilo hartomo <encusilo545110@gmail.com>
2020-07-15 19:52:41 +0700

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,37 +1,99 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { login, register } from '../store/actions/userAction'
+import { Link, useHistory } from 'react-router-dom'
 
 function Login() {
+    const dispatch = useDispatch()
+    const history = useHistory()
     const [email, setEmail] = useState("")
     const [pwd, setPwd] = useState("")
 
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-5">
+    const [page, setPage] = useState('login')
 
+    const onHandleLogin = () => {
+        history.push('/home')
+        dispatch(login({
+            email,
+            password: pwd
+        }))
+    }
 
-                    <form>
-                        <div className="form-group">
-                            <label >Email address</label>
-                            <input type="email" className="form-control"
-                                onChange={(e) => setEmail(e.target.value)} />
+    {
+        // page login
+        if (page === "login") {
+            return (
+                <>
+                    <div className="h-100">
+                        <div className="container">
+                            <div className="row w-100 justify-content-center align-middle">
+                                <div className="col-5">
+                                    <div className="card p-4">
+                                        <form>
+                                            <div className="form-group">
+                                                <label >Email address</label>
+                                                <input type="email" className="form-control"
+                                                    onChange={(e) => setEmail(e.target.value)} />
 
-                            <small id="emailHelp" className="form-text text-muted">Input with email format, thanks!</small>
+                                                <small id="emailHelp" className="form-text text-muted">Input with email format, thanks!</small>
+                                            </div>
+                                            <div className="form-group">
+                                                <label >Password</label>
+                                                <input type="password" className="form-control" onChange={(e) => setPwd(e.target.value)} />
+                                            </div>
+                                            <div className="form-group form-check">
+                                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                                <label className="form-check-label" for="exampleCheck1">Remember account</label>
+                                            </div>
+                                            <Link to="/home" onClick={onHandleLogin} type="button" className="btn btn-primary">LOGIN</Link>
+                                        </form>
+                                        <a onClick={() => setPage('register')} href="true">Register Now</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="form-group">
-                            <label >Password</label>
-                            <input type="password" className="form-control" onChange={(e) => setPwd(e.target.value)} />
+                    </div>
+                </>
+            )
+        }
+        else {
+            // page register
+            return (
+                <>
+                    <div className="h-100">
+                        <div className="container">
+                            <div className="row w-100 justify-content-center align-middle">
+                                <div className="col-5">
+                                    <div className="card p-4">
+                                        <form>
+                                            <div className="form-group">
+                                                <label >Email address</label>
+                                                <input type="email" className="form-control"
+                                                    onChange={(e) => setEmail(e.target.value)} />
+
+                                                <small id="emailHelp" className="form-text text-muted">Input with email format, thanks!</small>
+                                            </div>
+                                            <div className="form-group">
+                                                <label >Password</label>
+                                                <input type="password" className="form-control" onChange={(e) => setPwd(e.target.value)} />
+                                            </div>
+                                            <div className="form-group form-check">
+                                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                                <label className="form-check-label" for="exampleCheck1">Remember account</label>
+                                            </div>
+                                            <button onClick={onHandleLogin} type="button" className="btn btn-primary">LOGIN</button>
+                                        </form>
+                                        <a onClick={() => setPage('login')} href="true">Have account, Login Now</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="form-group form-check">
-                            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                            <label className="form-check-label" for="exampleCheck1">Remember account</label>
-                        </div>
-                        <button type="submit" className="btn btn-primary">LOGIN</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
+                    </div>
+                </>
+            )
+        }
+    }
 }
 
 export default Login
