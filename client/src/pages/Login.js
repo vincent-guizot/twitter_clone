c354654775daa58513cb748df27a81caab904a81
Vincent Sadino Guizot <vincentguizot128@gmail.com>
2020-07-16 23:09:20 +0700

    css auth

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -31,7 +31,7 @@ function Login() {
             image_url: image
         }))
     }
-
+    
     if (localStorage.getItem("access_token")) return <Redirect to="/" />
 
     {
@@ -39,11 +39,11 @@ function Login() {
         if (page === "login") {
             return (
                 <>
-                    <div className="h-100">
-                        <div className="container">
-                            <div className="row w-100 justify-content-center align-middle">
-                                <div className="col-5">
-                                    <div className="card p-4">
+                    <div className="w-100 h-100 d-flex justify-content-center align-items">
+                            <div className="row w-75 ">
+                                <div className="col-5 bg-info"></div>
+                                <div className="col-7 m-auto">
+                                    <div className="w-75 p-4">
                                         <form>
                                             <div className="form-group">
                                                 <label >Email address</label>
@@ -61,11 +61,10 @@ function Login() {
                                             </div>
                                             <button onClick={onHandleLogin} type="button" className="btn btn-block btn-primary">LOGIN</button>
                                         </form>
-                                        <button onClick={() => setPage('register')} >Register Now</button>
+                                        <button className="btn btn-link mt-3 font-style" onClick={() => setPage('register')} >Register Now</button>
                                     </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
                 </>
             )
